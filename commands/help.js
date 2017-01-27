1c2b449e2eb909126ea87f57f520906ce26d09e1
robflop <robflop98@web.de>
2017-01-27 19:31:47 +0100

    Change help command output

--- commands/help.js
+++ commands/help.js
@@ -18,10 +18,10 @@ exports.main = function(selfbot, msg, msgArray) { // Export command's function
     };
     for(var i = 0; i < cmdList.length; i++) { 
 	// Loop through each command (key)
-		commandsExpl.push(`'${cmdList[i]}' -- ${Commands.commands[cmdList[i]].desc}`);
+		commandsExpl.push(`\`\`'${cmdList[i]}' -- ${Commands.commands[cmdList[i]].desc}\`\``);
 		// Push each command including its description into the commandsExpl array
 	}; 
-    msg.edit(`**__Available commands are:__**\n\n${commandsExpl.join("\n")}\n\nUse '${config.commandPrefix + command} <commandname>' to get syntax help on a command!\n\n**Disabled commands are: ${disabledCommands.join(", ")}**`).then(msg => msg.delete(30000));
+    msg.edit(`**__Available commands are:__**\n\n${commandsExpl.join("\n")}\n\n\`\`Use '${config.commandPrefix + command} <commandname>' to get syntax help on a command!\`\`\n\n**Disabled commands are: ${disabledCommands.join(", ")}**`).then(msg => msg.delete(30000));
     // Send the command list and the disabled commands list and then set auto-delete to 30s.
 };
 
