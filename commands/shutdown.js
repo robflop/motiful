9158f16f71309ea77ef94a6f620293ccf31c5137
robflop <robflop98@gmail.com>
2017-02-12 13:23:40 +0100

    Fix shutdown command

--- commands/shutdown.js
+++ commands/shutdown.js
@@ -5,7 +5,7 @@ exports.main = function(selfbot, msg, msgArray) { // Export command function
 	msg.edit("Selfbot shutting down!").then(msg => msg.delete(1000));
 	// Edit the command call and set auto-delete to 1s
 	setTimeout(function(){
-		selfbot.destroy().then(console.log("Motiful signed out!"); process.exit(0)); 
+		selfbot.destroy().then(()=>{console.log("Motiful signed out!"); process.exit(0)}); 
 		// Sign out motiful and then end the node process
 	}, 2000); 
 	// Set timeout for shutting down the selfbot to 2sec after the command is triggered.
