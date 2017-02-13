c6f5243c605f81965e8709ae1f52992bcf94d9f9
robflop <robflop98@gmail.com>
2017-02-13 22:51:52 +0100

    Fix status when actual user is offline

--- event_handler.js
+++ event_handler.js
@@ -4,6 +4,12 @@ const moment = require('moment'); // Part of log writing
 module.exports = { // Export event functions
 	"ready": function ready(selfbot) { // Once the selfbot is ready (fully booted) ...
 		console.log(`[${moment().format('DD/MM/YYYY HH:mm:ss')}] Motiful ready!`); // ...console log a ready message.
+		selfbot.user.setStatus('invisible');
+		/*
+		Set motiful's status to invisible, so you won't appear as online if you actually aren't but have motiful running.
+		If you ARE online, the online/idle/dnd status of your discord client will overweigh motiful's invisible status, meaning
+		that this won't make you permanently display as invisible.
+		*/
 	},
 	"error": function error(selfbot) { // If a "serious connection error" occurs...
 		console.log(`[${moment().format('DD/MM/YYYY HH:mm:ss')}] Motiful encountered a "serious connection error"!`); // ...console log a notifcation.	
