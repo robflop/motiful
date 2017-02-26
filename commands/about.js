5de35687bd101b0111ae7f41a3db20ae09a81b50
robflop <robflop98@web.de>
2017-02-26 13:58:20 +0100

    nitro command update

--- commands/about.js
+++ commands/about.js
@@ -1,6 +1,6 @@
 exports.main = function(selfbot, msg, msgArray, chalk) { // Export command function
 	var command = "about";
-	msg.edit(`Motiful made by robflop#2174. Made mostly for easy (custom) emotes.\nCheck out the Github repo at <https://github.com/robflop/motiful>.`);
+	msg.edit(`Motiful made by robflop#0200. Made mostly for easy (custom) emotes.\nCheck out the Github repo at <https://github.com/robflop/motiful>.`);
 };
 
 exports.desc = "Get general info about motiful"; // Export command description
