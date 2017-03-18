5ac44cf071bc4002f9a092ef5dc17ecb3c2827cd
robflop <robflop98@web.de>
2017-03-19 00:50:17 +0100

    comment removal

--- event_handler.js
+++ event_handler.js
@@ -1,24 +1,20 @@
-const config = require('./userconfig/config.json'); // Import configuration
-const moment = require('moment'); // Part of log writing
+const config = require('./userconfig/config.json');
+const moment = require('moment');
 var timestamp = moment().format('DD/MM/YYYY HH:mm:ss');
 
-module.exports = { // Export event functions
-	"ready": function ready(selfbot, chalk) { // Once the selfbot is ready (fully booted) ...
-		console.log(`[${timestamp}]${chalk.green("[POWER]")} motiful ready!`); // ...console log a ready message.
+module.exports = {
+	"ready": function ready(selfbot, chalk) {
+		console.log(`[${timestamp}]${chalk.green("[POWER]")} motiful ready!`);
 		selfbot.user.setStatus('invisible');
-		/*
-		Set motiful's status to invisible, so you won't appear as online if you actually aren't but have motiful running.
-		If you ARE online, the online/idle/dnd status of your discord client will overweigh motiful's invisible status, meaning
-		that this won't make you permanently display as invisible.
-		*/
+		// appear offline when not online as actual user
 	},
-	"error": function error(selfbot, error, chalk) { // If a "serious connection error" occurs...
+	"error": function error(selfbot, error, chalk) {
 		timestamp = moment().format('DD/MM/YYYY HH:mm:ss');
-		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} motiful encountered a "serious connection error! | ${error.code}`); // ...console log a notifcation.
+		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} motiful encountered a "serious connection error! | ${error.code}`);
 	},
-	"disconnect": function disconnect(selfbot, error, chalk) { // If the selfbot gets disconnected...
+	"disconnect": function disconnect(selfbot, error, chalk) {
 		timestamp = moment().format('DD/MM/YYYY HH:mm:ss');
-		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} motiful was disconnected! | ${error.code}`); // ...console log a notifcation.
+		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} motiful was disconnected! | ${error.code}`);
 		if(error.code == 1000) {
 			console.log(`[${timestamp}]${chalk.green("[POWER]")} Automatically restarting...`);
 			selfbot.destroy().then(() => selfbot.login(config.token));
