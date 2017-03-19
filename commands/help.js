59445df3b71725089f811711b3bc5ef080d525c2
robflop <robflop98@gmail.com>
2017-03-19 19:02:37 +0100

    Fix help arguments

--- commands/help.js
+++ commands/help.js
@@ -9,7 +9,7 @@ exports.main = function(selfbot, msg, msgArray, chalk) {
     var cmdList = Object.keys(Commands.commands);
     var arg = msgArray[1];
     // possible arg to get help on a command
-    if(arg && cmdList.indexOf(arg) !== -1) return msg.edit(`**__Syntax for '${arg}' is:__** \`\`\`${config.commandPrefix + arg + " " + Commands.commands[arg].syntax}\`\`\``).then(msg => msg.delete(20000));
+    if(arg && cmdList.indexOf(arg) !== -1) return msg.channel.sendMessage(`**__Syntax for '${arg}' is:__** \`\`\`${config.commandPrefix + arg + " " + Commands.commands[arg].syntax}\`\`\``).then(msg => msg.delete(20000));
     for(var i = 0; i < cmdList.length; i++) commandsExpl.push(`\`\`'${cmdList[i]}' -- ${Commands.commands[cmdList[i]].desc}\`\``);
     msg.channel.sendMessage(`**__Available commands are:__**\n\n${commandsExpl.join("\n")}\n\n\`\`Use '${config.commandPrefix + command} <commandname>' to get syntax help on a command!\`\`\n\n**${disabledCommands.join(", ").length>0 ? "Disabled commands: " + disabledCommands.join(", ") : "No commands have been disabled."}**`, {split: {char: "\n"}});
 };
