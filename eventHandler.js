b56dadde9d650bf8cea1041b3b512aa1feb24692
robflop <robflop98@gmail.com>
2017-04-22 22:39:15 +0200

    exit process if pm2 used instead of re-login

--- eventHandler.js
+++ eventHandler.js
@@ -17,7 +17,7 @@ module.exports = {
 		console.log(`[${timestamp}]${chalk.red("[CONNECTION]")} motiful was disconnected! | ${error.code}`);
 		if(error.code == 1000) {
 			console.log(`[${timestamp}]${chalk.green("[POWER]")} Automatically restarting...`);
-			client.destroy().then(() => client.login(config.token));
+			config.pm2 ? process.exit(0) : client.destroy().then(() => client.login(config.token));
 			// Restart selfbot if disconnect code is 1000 (gracefully exited) because it won't reconnect automatically
 		};
 	},
