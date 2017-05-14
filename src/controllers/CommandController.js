9f75d0bf8df342cc731866b846b8e922100fe2c9
robflop <robflop98@web.de>
2017-05-14 17:11:12 +0200

    Start of rewrite - general structure etc

---
+++ src/controllers/CommandController.js
@@ -0,0 +1,66 @@
+const { inspect } = require('util');
+const { Collection } = require('discord.js');
+
+require('../util/prototypes');
+const ArgumentParser = require('./ArgumentParser');
+
+class CommandController {
+	constructor() {
+		this.disabledCommands = require('../data/disabledCommands.json');
+	}
+
+	async handleCommand(message) {
+		if (message.author.id !== config.ownerID || !message.content.startsWith(config.commandPrefix)) return;
+
+		const { commands, aliases, config, logger } = message.client;
+
+		const [name, ...args] = message.content.replace(config.commandPrefix, '').trim().split(/ +/g);
+		const command = commands.get(name.toLowerCase()) || commands.get(aliases.get(name.toLowerCase()));
+
+		if (!command) return;
+
+		if (command.guildOnly && message.channel.type !== 'text') {
+			return message.edit(`The ${command.name} command is only available on servers.`).then(msg => msg.delete(3000));
+		}
+
+		if (this.disabledCommands.includes(command.name)) return;
+
+		if (command.args.length && args.length < command.args.length && !('defaultVal' in command.args.last())) {
+			const correctSyntax = `${config.commandPrefix} ${command.name} ${command.args.map(a => `<${a.name}>`).join(' ')}`;
+			return message.edit(`you didn't provide enough arguments! The correct format would be:\n\`${correctSyntax}\``)
+			.then(msg => msg.delete(3000));
+		}
+
+		const parsedArgs = command.args.length ? await this.parseArguments(args, command, message) : args;
+
+		return command.run(message, parsedArgs).catch(e => {
+			logger.error(inspect(e));
+			return message.edit(`an error occurred while executing the \`${command.name}\` command.`).then(msg => msg.delete(3000));
+		});
+	}
+
+	async parseArguments(args, command, message) {
+		const parsedArgs = {};
+		const beginning = args.slice(0, command.args.length - 1);
+		const end = args.slice(beginning.length, args.length).join(' ');
+
+		for (const [i, arg] of beginning.concat(end).entries()) {
+			const { name, type, defaultVal } = command.args[i];
+
+			parsedArgs[name] = 'defaultVal' in command.args[i] && !arg ? defaultVal : ArgumentParser.parse(type, message, arg);
+
+			if (parsedArgs[name] instanceof Promise) {
+				parsedArgs[name] = await parsedArgs[name];
+			}
+
+			if (parsedArgs[name] === null) {
+				message.edit(`invalid input for the \`${name}\` argument! Expected \`${type}\`.`).then(msg => msg.delete(3000));
+				return undefined;
+			}
+		}
+
+		return parsedArgs;
+	}
+}
+
+module.exports = CommandController;
---
+++ src/motiful.js
@@ -0,0 +1,30 @@
+const MotifulClient = require('./structures/MotifulClient');
+const CommandController = require('./controllers/CommandController');
+const DiscordAPIError = require('discord.js/src/client/rest/DiscordAPIError');
+
+const client = new MotifulClient();
+const controller = new CommandController();
+
+client
+	.once('ready', () => {
+		client.logger.info('motiful ready!');
+	})
+	.on('message', message => {
+		controller.handleCommand(message);
+	})
+	.login(client.config.token);
+
+process.on('unhandledRejection', err => {
+	if (!err) return;
+
+	const errorString = 'Uncaught Promise Error:\n';
+	if (err instanceof DiscordAPIError) {
+		return client.logger.error(`${errorString}${err.code} - ${err.message}`);
+	}
+
+	if (err.method && err.path && err.text) {
+		return client.logger.error(`${errorString}${err.method}: ${err.path}\n${err.text}`);
+	}
+
+	return client.logger.error(errorString + err.stack);
+});
