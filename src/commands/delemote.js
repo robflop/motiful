121bbcbb9124b0ad7f457bdeb9af0ae552010735
robflop <robflop98@web.de>
2017-05-20 16:38:28 +0200

    Delemote adjustment

--- src/commands/delemote.js
+++ src/commands/delemote.js
@@ -17,12 +17,12 @@ class DelEmoteCommand extends Command {
 	}
 
 	async run(message, args) {
-		let emotePath = join(__dirname, '..', 'data', 'customemotes');
+		const emoteFolder = join(__dirname, '..', 'data', 'customemotes');
 		const extensions = ['png', 'jpg', 'gif', 'jpeg', 'webp'];
 		const emoteRegex = new RegExp(`${args.emotename}.(${extensions.join('|')})`);
-		const emote = readdirSync(emotePath).filter(file => emoteRegex.test(file))[0] || '';
+		const emote = readdirSync(emoteFolder).filter(file => emoteRegex.test(file))[0] || '';
 		if (emote) {
-			emotePath = join(emotePath, emote);
+			const emotePath = join(emoteFolder, emote);
 			unlink(emotePath, err => {
 				if (err) return message.edit(`Error deleting emote!: \`\`\`${err}\`\`\``).then(msg => msg.delete(3000));
 				else return message.edit(`Successfully deleted emote \`${args.emotename}\`!`).then(msg => msg.delete(3000));
