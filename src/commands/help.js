b411ee27cdfc70e0e8d787279d6917c5f72d907e
robflop <robflop98@gmail.com>
2017-05-20 16:46:00 +0200

    extremely helpful help command

---
+++ src/commands/help.js
@@ -0,0 +1,14 @@
+const Command = require('../structures/Command');
+
+class HelpCommand extends Command {
+	constructor() {
+		super({
+			name: 'help',
+			description: 'Get usage help'
+		});
+	}
+
+	async run(message, args) {
+		return message.edit('Refer to https://github.com/robflop/motiful/wiki/Usage for extensive Usage help.').then(msg => msg.delete(5000));
+	}
+}
