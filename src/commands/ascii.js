578501235480569f89955acc7bd1cd58eb1d7e78
robflop <robflop98@gmail.com>
2017-05-20 03:34:36 +0200

    never enough

--- src/commands/ascii.js
+++ src/commands/ascii.js
@@ -19,15 +19,15 @@ class AsciiCommand extends Command {
 	async run(message, args) {
 		const { logger } = message.client;
 		snekfetch.get(`http://artii.herokuapp.com/make?text=${args.input}`)
-		.then(ascii => {
-			if (ascii.text.length > 1999) return message.edit('Output too long. Try shorter text.').then(message => message.delete(2000));
-			return message.edit(`\`\`\`${ascii.text}\`\`\``);
-		})
-		.catch(err => {
-			const errorDetails = `${err.host ? err.host : ''} ${err.message ? err.message : ''}`.trim();
-			message.edit(`An error occurred sending the request: \`${err.code}: ${errorDetails}\``).then(msg => msg.delete(3000));
-			return logger.error(inspect(err));
-		});
+			.then(ascii => {
+				if (ascii.text.length > 1999) return message.edit('Output too long. Try shorter text.').then(message => message.delete(2000));
+				return message.edit(`\`\`\`${ascii.text}\`\`\``);
+			})
+			.catch(err => {
+				const errorDetails = `${err.host ? err.host : ''} ${err.message ? err.message : ''}`.trim();
+				message.edit(`An error occurred sending the request: \`${err.code}: ${errorDetails}\``).then(msg => msg.delete(3000));
+				return logger.error(inspect(err));
+			});
 	}
 }
 
