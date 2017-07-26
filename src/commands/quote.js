a14e9693057793bcdd5f6ca031fd0eaa1d58fc48
robflop <robflop98@web.de>
2017-07-26 20:13:33 +0200

    adjust quoting

--- src/commands/quote.js
+++ src/commands/quote.js
@@ -32,8 +32,9 @@ class QuoteCommand extends Command {
 			snippet = args.input.trim();
 		}
 		message.channel.fetchMessages({ limit: 100 }).then(messages => {
+			messages.delete(message.id); // delete command call from collection
 			const quoteMsg = messages.filter(msg => msg.author.id === args.user.id && msg.content.toLowerCase().includes(snippet)).first();
-			if (!quoteMsg) return message.channel.send('Message not found!').then(msg => msg.delete(2000));
+			if (!quoteMsg) return message.edit('Message not found!').then(msg => msg.delete(2000));
 			const date = moment(quoteMsg.createdTimestamp).format('Do MMM YYYY'), time = moment(quoteMsg.createdTimestamp).format('HH:mm:ss');
 			const name = quoteMsg.author.username, avatar = quoteMsg.author.avatarURL;
 			const embed = new RichEmbed()
