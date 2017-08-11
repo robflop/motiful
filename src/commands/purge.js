deb71652c3db9c95806b7bf81d217fa2a648fefe
robflop <robflop98@web.de>
2017-08-11 11:23:18 +0200

    ownerID is ded

--- src/commands/purge.js
+++ src/commands/purge.js
@@ -18,7 +18,7 @@ class PurgeCommand extends Command {
 	async run(message, args) {
 		await message.delete();
 		message.channel.fetchMessages({ limit: 100 }).then(messages => {
-			messages = messages.filterArray(message => message.author.id === message.client.config.ownerID).slice(0, args.amount);
+			messages = messages.filterArray(message => message.author.id === message.client.user.id).slice(0, args.amount);
 			for (const message of messages) message.delete();
 		});
 	}
