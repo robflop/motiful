a1d357e0290d4589650c2b9aba176df73250d320
robflop <robflop98@gmail.com>
2017-09-01 18:35:11 +0200

    check for author id to be user's id

--- src/commands/sub.js
+++ src/commands/sub.js
@@ -19,11 +19,17 @@ class SubCommand extends Command {
 		const toReplace = args.input.substring(0, args.input.indexOf('|') - 1).trim().split(';');
 		const replaceWith = args.input.substring(args.input.indexOf('|') + 1).trim().split(';');
 		await message.delete();
+
 		message.channel.fetchMessages({ limit: 100 }).then(messages => {
-			const targetMessage = messages.filter(msg => toReplace.every(word => msg.content.toLowerCase().includes(word.toLowerCase()))).first();
+			const targetMessage = messages.filter(msg => {
+				msg.author.id === msg.client.user.id;
+				return toReplace.every(word => msg.content.toLowerCase().includes(word.toLowerCase()));
+			}).first();
+
 			for (let i = 0; i < toReplace.length; i++) {
 				targetMessage.content = targetMessage.content.replace(toReplace[i], replaceWith[i]);
 			}
+
 			return targetMessage.edit(targetMessage.content);
 		});
 	}
