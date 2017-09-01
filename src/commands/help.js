bd5cd93fd66fe599517e9bce33ba63d1021143bd
robflop <robflop98@gmail.com>
2017-09-01 18:07:57 +0200

    Link to command's help section

--- src/commands/help.js
+++ src/commands/help.js
@@ -6,11 +6,19 @@ class HelpCommand extends Command {
 			name: 'help',
 			description: 'Get usage help',
 			aliases: ['h', 'commands'],
+			args: [
+				{
+					type: 'string',
+					name: 'command',
+					defaultVal: ''
+				}
+			]
 		});
 	}
 
 	async run(message, args) {
-		return message.edit('Refer to https://github.com/robflop/motiful/wiki/Usage for extensive Usage help.').then(msg => msg.delete(5000));
+		return message.edit(`Refer to https://github.com/robflop/motiful/wiki/Usage#${args.command} for extensive Usage help.`)
+			.then(msg => msg.delete(5000));
 	}
 }
 
